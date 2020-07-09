6dc48dff3406f585ede75c0578eabaf84758ad17
Sabahat Iqbal <sabbyiqbal@gmail.com>
2020-07-09 15:02:42 -0400

    Updated treemap; need help

--- js/main-pk-budget.js
+++ js/main-pk-budget.js
@@ -1,21 +1,14 @@
 // set the dimensions and margins of the graph
-let margin = { top: 10, right: 30, bottom: 50, left: 60 },
-  width = 954 - margin.left - margin.right,
-  height = 954 - margin.top - margin.bottom;
+let margin = { top: 10, right: 10, bottom: 10, left: 10 },
+  width = 900;
+height = 700;
 
 const color = d3.scaleOrdinal(d3.schemeCategory10);
 
-const format = d3.format(",d");
-
-//Read the data
-//Loading into XLS from here: http://www.finance.gov.pk/fb_2019_20.html
-//Zoomable tree-map: https://observablehq.com/@d3/zoomable-treemap
-//Useful SO post: https://stackoverflow.com/questions/56281711/treemap-in-d3-v5-doesnt-support-nodes-anymore
 d3.csv("data/data.csv").then(function (results) {
   //Update the "Rs,millions" value to be a number:
   for (let index = 0; index < results.length; index++) {
     const element = results[index];
-    console.log(element);
 
     for (const property in element) {
       if (property === "Rs,millions") {
@@ -35,10 +28,8 @@ d3.csv("data/data.csv").then(function (results) {
       return d.Parent;
     })(results);
 
-  console.log(dataStratified);
-
   let myTreemap = (data) =>
-    d3.treemap().padding(1).round(true)(
+    d3.treemap().size([width, height]).padding(1).round(true)(
       d3
         .hierarchy(data)
         .sum((d) => d.data["Rs,millions"])
@@ -52,32 +43,48 @@ d3.csv("data/data.csv").then(function (results) {
   const svg = d3
     .select("#my_dataviz")
     .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .style("font-family", "sans-serif")
+    .attr("width", width)
+    .attr("height", height);
 
-  const leaf = svg
-    .selectAll("g")
+  const g = svg.append("g").attr("class", "treemap-container");
+
+  // Place the labels for our countries
+  g.selectAll("text.budgetline")
+    // The data is the first "generation" of children
+    .data(root.children)
+    .enter()
+    .append("text")
+    .attr("class", "budgetline")
+    // The rest is just placement/styling
+    .attr("x", (d) => d.x0)
+    .attr("y", (d) => d.y0)
+    .attr("dy", "0.6em")
+    .attr("dx", 3)
+    .style("font-size", 12)
+    // Remember, the data on the original node is available on node.data
+    .text((d) => d.data.data.Child);
+
+  // Now, we place the groups for all of the leaf nodes
+  const leaf = g
+    .selectAll("g.leaf")
+    // root.leaves() returns all of the leaf nodes
     .data(root.leaves())
     .enter()
     .append("g")
-    .attr("transform", (d) => `translate(${d.x0},${d.y0})`);
+    .attr("class", "leaf")
+    // position each group at the top left corner of the rect
+    .attr("transform", (d) => `translate(${d.x0},${d.y0})`)
+    .style("font-size", 10);
 
   console.log(root.leaves());
 
-  // leaf.append("title").text((d) =>
-  //   console.log(
-  //     d
-  //       .ancestors()
-  //       .reverse()
-  //       .map((each) => each.data)
-  //   )
-  // );
+  // A title element tells the browser to display its text value
+  // in a popover when the cursor is held over a rect. This is a simple
+  // way to add some interactivity
+  leaf.append("title").text((d) => d.data.id);
 
-  // leaf.append("title")
-  //     .text(d => `${d.ancestors().reverse().map(d => d.data.name).join("/")}\n${format(d.value)}`);
-
+  // Now we append the rects.
   leaf
     .append("rect")
     .attr("id", (d) => d.data.id)
@@ -85,43 +92,44 @@ d3.csv("data/data.csv").then(function (results) {
       while (d.depth > 1) d = d.parent;
       return color(d.data.data.Child);
     })
-    .attr("fill-opacity", 0.6)
-    .attr("width", function (d) {
-      console.log(d);
-      console.log(d.x1, d.x0);
-      // return d.x1 - d.x0
-    })
-    .attr("height", (d) => d.y1 - d.y0);
+    .attr("opacity", 0.7)
+    // the width is the right edge position - the left edge position
+    .attr("width", (d) => d.x1 - d.x0)
+    // same for height, but bottom - top
+    .attr("height", (d) => d.y1 - d.y0)
+    // make corners rounded
+    .attr("rx", 3)
+    .attr("ry", 3);
 
-  leaf.append("clipPath").append("use");
+  // This next section checks the width and height of each rectangle
+  // If it's big enough, it places labels. If not, it doesn't.
+  leaf.each((d, i, arr) => {
+    // The current leaf element
+    const current = arr[i];
 
-  //start here: something showed up on page; but what are clip-paths?
-  //https://developer.mozilla.org/en-US/docs/Web/CSS/clip-path
+    const left = d.x0,
+      right = d.x1,
+      // calculate its width from the data
+      width = right - left,
+      top = d.y0,
+      bottom = d.y1,
+      // calculate its height from the data
+      height = bottom - top;
 
-  //Other possibility:
-  //build JSON file manually, then follow Observable for zoomable treemap.
+    // too small to show text
+    const tooSmall = width < 24 || height < 15;
 
-  leaf
-    .append("text")
-    .selectAll("tspan")
-    .data((d) =>
-      d.data.data.Child.split(/(?=[A-Z][a-z])|\s+/g).concat(
-        format(d.data["Rs,millions"])
-      )
-    )
-    .enter()
-    .append("tspan")
-    .attr("x", 3)
-    .attr(
-      "y",
-      (d, i, nodes) => `${(i === nodes.length - 1) * 0.3 + 1.1 + i * 0.9}em`
-    )
-    .attr("fill-opacity", (d, i, nodes) =>
-      i === nodes.length - 1 ? 0.7 : null
-    )
-    .text((d) => d);
+    // and append the text
+    d3.select(current)
+      .append("text")
+      // If it's too small, don't show the text
+      .attr("opacity", tooSmall ? 0 : 0.9)
+      .selectAll("tspan")
+      .data((d) => [d.data.data.Child, d.value.toLocaleString()])
+      .enter()
+      .append("tspan")
+      .attr("x", 3)
+      .attr("y", (d, i) => (i ? "2.5em" : "1.15em"))
+      .text((d) => d);
+  });
 });
-
-// d3.json("data/flare-2.json").then(function (data) {
-//   console.log(data);
-// });
